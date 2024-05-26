ad328a07a6a802736701a5c967b9f6ee8bbaec48
Nathanael Caraúna <51246457+NathanaelCarauna@users.noreply.github.com>
2024-05-26 09:55:17 -0300

    Time: 40 ms (95.85%) | Memory: 17.3 MB (84.74%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,8 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:        
         for i in range(len(nums)):
-            curr = nums[i]
-            print("Current " + str(curr), "Target: " + str(target))
+            curr = nums[i]            
             if curr >= target :
                 return i            
         return len(nums)
